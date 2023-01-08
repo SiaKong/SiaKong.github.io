0d53407fe4030e98052efa83163007ad0d46dc2b
Coding0116 <sianabinkong@gmail.com>
2023-01-08 16:59:11 -0500

    about page skeleton

--- myweb/src/pages/About.js
+++ myweb/src/pages/About.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import FadeIn from 'react-fade-in';
 
 function About() {
 
   return (
     <div name='about' id='about' className='w-full h-screen bg-[#00161a]'>
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <p className="text-4xl text-[#acdfe8] p-6">ABOUT ME</p>
 
+        {/*About Profile image*/}
+        <div class="text-center pb-7">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp"
+            className="rounded-full w-44 mb-4 mx-auto"
+            alt="Profile"
+          />
+          <h5 class="text-xl font-medium leading-tight mb-2 text-[#5ab4c4]">SIANA KONG</h5>
+          <p class="text-[#bbc0c0]">Web designer</p>
+        </div>
+
+        {/*About Text*/}
+        <div class="box-border p-4 border-4 rounded-lg border-[#acdfe8]">
+          <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
+            <p className="text-white">this is left ahasdfasdfadf</p>
+            <p className="text-white">and this is right adsfadsfasdf</p>
+          </div>
+          
+        </div>
+        
+        
+      </div>
     </div>
   )
 }
--- myweb/src/pages/Home.js
+++ myweb/src/pages/Home.js
@@ -21,22 +21,25 @@ function Home() {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full items-center text-center">
         <FadeIn transitionDuration={1300}> 
           <>
-          <h1 className="text-5xl sm:text-[90px] text-[#00161a] pb-3">SIANA KONG</h1>
-          <p className="text-sm sm:text-[20px] font-bold text-[#005e6e] pb-3"><mark>&nbsp;SOFTWARE DEVELOPER STUDENT&nbsp;</mark></p>
-          <p className="text-sm sm:text-lg">Welcome to my portfolio website</p>
-          <p className="text-sm sm:text-lg pb-6">the next line</p>
-          <p className="text-sm sm:text-[16px] pb-2 font-bold text-[#005e6e]">• CONTACT •</p>
-          <FloatingButtons 
-            buttonType='hori-dots'
-            dimension={40}
-            degree={180} 
-            distance={100} 
-            buttonsList={buttonsList} 
-            top={'calc(50% + 120px)'} 
-            left={"calc(50% - 22px)"}  
-            direction="circular" />
+            {/*Text Components*/}
+            <h1 className="text-5xl sm:text-[90px] text-[#00414d] pb-3">SIANA KONG</h1>
+            <p className="text-sm sm:text-[20px] pb-10 text-[#808080]">&nbsp;SOFTWARE DEVELOPER STUDENT&nbsp;</p>
+            <p className="text-sm sm:text-[16px] pt-4 font-semibold text-[#007c91]">• CONTACT •</p>
+            <>
+
+            {/*Contact buttons*/}
+            <FloatingButtons 
+              buttonType='hori-dots'
+              dimension={40}
+              degree={180} 
+              distance={100} 
+              buttonsList={buttonsList} 
+              top={'calc(50% + 120px)'} 
+              left={"calc(50% - 22px)"}  
+              direction="circular" />
             </>
-          </FadeIn>
+          </>
+        </FadeIn>
       </div>
     </div>
   );
